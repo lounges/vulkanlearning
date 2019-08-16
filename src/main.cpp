37ca483d3d353a24a7fc089d718357672518e711
Joshua Martin <josh@hive05.com>
2019-08-16 15:34:57 -0700

    Added window creation/cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,72 @@
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <stdexcept>
 #include <functional>
 #include <cstdlib>
 
+//Ready for this one:
+//https://vulkan-tutorial.com/en/Drawing_a_triangle/Setup/Instance
+
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
 class HelloTriangleApplication {
 public:
-    void run() {
+    void run() 
+    {
+        initWindow(WIDTH, HEIGHT, "Vulkan");
         initVulkan();
         mainLoop();
         cleanup();
     }
 
 private:
-    void initVulkan() {
+    GLFWwindow* _window = nullptr;
+
+    void initWindow(int width, int height, const char* title) 
+    {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        _window = glfwCreateWindow(width, height, title, nullptr, nullptr);
+    }
+
+    void initVulkan() 
+    {
 
     }
 
-    void mainLoop() {
-
+    void mainLoop()
+    {
+        while (!glfwWindowShouldClose(_window)) {
+            glfwPollEvents();
+        }
     }
 
-    void cleanup() {
-
+    void cleanup() 
+    {
+        if( _window != nullptr)
+        {
+            glfwDestroyWindow(_window);
+            _window = nullptr;
+        }
+            
+        glfwTerminate();
     }
 };
 
-int main() {
+int main() 
+{
     HelloTriangleApplication app;
 
-    try {
+    try 
+    {
         app.run();
-    } catch (const std::exception& e) {
+    } 
+    catch (const std::exception& e) 
+    {
         std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
     }
