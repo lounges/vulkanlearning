17f1c88cdb45c9611537b9405c9bf2238a76fcb2
Joshua Martin <josh@hive05.com>
2019-08-20 15:22:05 -0700

    Setup imageviews for the swap chain images

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@
 #include <algorithm>
 
 //Ready for this one:
-//https://vulkan-tutorial.com/en/Drawing_a_triangle/Presentation/Image_views
+//https://vulkan-tutorial.com/en/Drawing_a_triangle/Graphics_pipeline_basics/Introduction
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
@@ -100,6 +100,7 @@ private:
     VkSurfaceKHR _surface = VK_NULL_HANDLE;
     VkSwapchainKHR _swapChain = VK_NULL_HANDLE;
     std::vector<VkImage> _swapChainImages;
+    std::vector<VkImageView> _swapChainImageViews;
     VkFormat _swapChainImageFormat;
     VkExtent2D _swapChainExtent;
 
@@ -129,12 +130,43 @@ private:
         pickPhysicalDevice();
         createLogicalDevice();
         createSwapChain();
+        createImageViews();
     }
 
     void createSurface() 
     {
         if (glfwCreateWindowSurface(_instance, _window, nullptr, &_surface) != VK_SUCCESS) 
             throw std::runtime_error("failed to create window surface!");
+    }
+
+    void createImageViews() 
+    {
+        _swapChainImageViews.resize(_swapChainImages.size());
+
+        for (auto i = 0; i < _swapChainImages.size(); i++) 
+        {
+            VkImageViewCreateInfo createInfo = {};
+            createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+            createInfo.image = _swapChainImages[i];
+
+            createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+            createInfo.format = _swapChainImageFormat;
+
+            //allows for remapping channels
+            createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+            createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+            createInfo.subresourceRange.baseMipLevel = 0;
+            createInfo.subresourceRange.levelCount = 1;
+            createInfo.subresourceRange.baseArrayLayer = 0;
+            createInfo.subresourceRange.layerCount = 1;
+
+            if (vkCreateImageView(_device, &createInfo, nullptr, &_swapChainImageViews[i]) != VK_SUCCESS) 
+                throw std::runtime_error("failed to create image views!");
+        }
     }
 
     void pickPhysicalDevice()
@@ -539,6 +571,10 @@ private:
 
     void cleanup() 
     {
+        for (auto imageView : _swapChainImageViews) {
+            vkDestroyImageView(_device, imageView, nullptr);
+        }
+
         if ( _swapChain != VK_NULL_HANDLE )
             vkDestroySwapchainKHR(_device, _swapChain, nullptr);
         
